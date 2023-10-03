1bbbe61ab0e2e4c694314ff3e7a35863368b02ee
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-10-03 22:44:26 +0700

    setup contact controller แบบ lazy load

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
+import 'package:contact_app/controllers/contact_controller.dart';
 import 'package:contact_app/pages/home_page/home_page.dart';
 import 'package:contact_app/pages/new_contact_page/new_contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:get/get.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    Get.lazyPut(
+      () {
+        return ContactController();
+      },
+    );
+
     return GetMaterialApp(
       title: 'Nextflow Contact App',
       theme: ThemeData(

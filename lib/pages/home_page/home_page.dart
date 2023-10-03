129c09ac750ecf273b8f7d0612107234600128b8
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-10-03 22:44:41 +0700

    เรียกใช้ list มาสร้างรายการผู้ติดต่อ

--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,8 +1,11 @@
+import 'package:contact_app/controllers/contact_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+  HomePage({super.key});
+
+  var contactController = Get.find<ContactController>();
 
   @override
   Widget build(BuildContext context) {
@@ -17,6 +20,19 @@ class HomePage extends StatelessWidget {
             icon: const Icon(Icons.add),
           ),
         ],
+      ),
+      // show listview of contacts from contact controller with Obx widget
+      body: Obx(
+        () => ListView.builder(
+          itemCount: contactController.contacts.length,
+          itemBuilder: (context, index) {
+            var contact = contactController.contacts[index];
+            return ListTile(
+              title: Text(contact.name),
+              subtitle: Text(contact.email),
+            );
+          },
+        ),
       ),
     );
   }

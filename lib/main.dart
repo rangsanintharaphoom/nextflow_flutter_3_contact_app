548d569825ab774364f0c8356e60122207c3a8fb
Teerasej Jiraphatchandej <teerasej@gmail.com>
2023-10-03 22:24:06 +0700

    เพิ่มสีพื้นหลัง

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.blue,
+          foregroundColor: Colors.white,
+        ),
       ),
       initialRoute: '/',
       getPages: [

51d3154170cceb63d92e9f32a3371b89ac8fa2f4
khunbumrung <webapp@tat.co.th>
2023-11-28 16:05:14 +0700

    เพิ่ม route page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:contact_app/pages/home_page/home_page.dart';
+import 'package:contact_app/pages/new_contact_page/new_contact_page.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -13,12 +14,27 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Nextflow Contact App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        //useMaterial3: true,
       ),
-      home: HomePage(),
+      initialRoute: "/",
+      getPages: [
+        GetPage(
+          name: "/",
+          page: () {
+            return const HomePage();
+          },
+        ),
+        GetPage(
+          name: "/new-contact",
+          page: () {
+            return NewContactPage();
+          },
+        ),
+        ],
     );
   }
 }

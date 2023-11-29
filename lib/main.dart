bd3582ca3f3c7051896864b85da8f5e7ddfe1253
siripat <siripat@tat.co.th>
2023-11-29 16:19:16 +0700

    แก้ bug

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         GetPage(
           name: '/',
           page: () {
-            return const HomePage();
+            return HomePage();
           },
         ),
         GetPage(

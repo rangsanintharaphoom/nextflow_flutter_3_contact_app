54994272b16d3a29bb8213744f8cd125acd0a28a
siripat <siripat@tat.co.th>
2023-11-29 16:20:06 +0700

    เพิ่ม การ put และ find controllor และ เพิ่ม obx แสดง alert message

--- lib/pages/new_contact_page/new_contact_page.dart
+++ lib/pages/new_contact_page/new_contact_page.dart
@@ -1,6 +1,10 @@
+import 'package:contact_app/pages/new_contact_page/new_contact_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class NewContactPage extends StatelessWidget {
+  var controller = Get.put(NewContactController());
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,21 +18,58 @@ class NewContactPage extends StatelessWidget {
             TextField(
               decoration: InputDecoration(
                 labelText: 'Username',
+                helperText: 'Up to 20 characters',
               ),
+              maxLength: 20,
+              onChanged: (value) {
+                controller.warningText.value = '';
+                controller.name = value;
+              },
             ),
             SizedBox(height: 16.0),
             TextField(
               decoration: InputDecoration(
                 labelText: 'EmailAddress',
+                helperText: 'Up to 20 characters',
               ),
+              maxLength: 20,
+              onChanged: (value) {
+                controller.email = value;
+              },
             ),
             SizedBox(height: 16.0),
+            Spacer(),
+            Obx(() {
+              // print(controller.warningText.value);
+              return Text('${controller.warningText.value}',
+                  style: TextStyle(color: Colors.red));
+            }),
+            // Text('${controller.warningText}',
+            //     style: TextStyle(color: Colors.red)),
             SizedBox(
               width: 120.0, // ตั้งค่าความกว้างที่คุณต้องการ
               height: 50.0, // ตั้งค่าความสูงที่คุณต้องการ
               child: ElevatedButton(
                 onPressed: () {
                   // Save button logic here
+
+                  // // ใช้ GetX แสดง dialog และเมื่อผู้ใช้กด OK จะเป็นการเรียกใช้ฟังก์ชัน save ใน NewContactController
+                  // Get.defaultDialog(
+                  //   title: 'Save Contact',
+                  //   middleText: 'Are you sure?',
+                  //   textConfirm: 'OK',
+                  //   textCancel: 'Cancel',
+                  //   onConfirm: () {
+                  //     controller.save();
+                  //     Get.back();
+                  //     Get.back();
+                  //   },
+                  //   onCancel: () {
+                  //     Get.back();
+                  //   },
+                  // );
+
+                  controller.save();
                 },
                 child: Text('Save'),
               ),
